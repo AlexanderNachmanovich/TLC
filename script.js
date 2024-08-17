3a039b656b8bcc5ebf99b6f7e6177ddab43a6524
AlexandrNachmanovich <a.nachmanovich@gmail.com>
2024-08-17 15:37:13 +0300

    demo v.0.5 mobile

--- script.js
+++ script.js
@@ -13,12 +13,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const mapContainer = document.getElementById("map-container");
   const zoomInBtn = document.getElementById("zoom-in");
   const zoomOutBtn = document.getElementById("zoom-out");
+
   let scale = 1;
   const maxScale = 1.8; // Максимальное увеличение до 80%
   const scaleFactor = 0.2;
   let isDragging = false;
   let startX, startY, initialX, initialY;
   let lastTouchEnd = 0;
+  let canZoom = true; // Флаг для задержки
 
   points.forEach((point) => {
     const x = parseFloat(point.getAttribute("data-x"));
@@ -66,106 +68,49 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   zoomInBtn.addEventListener("click", () => {
+    if (!canZoom) return;
     scale = Math.min(maxScale, scale + scaleFactor);
-    updateScale(mapContainer.clientWidth / 2, mapContainer.clientHeight / 2);
+    updateScale();
+    canZoom = false; // Блокируем дальнейшее увеличение на полсекунды
+    setTimeout(() => {
+      canZoom = true;
+    }, 500);
   });
 
   zoomOutBtn.addEventListener("click", () => {
+    if (!canZoom) return;
     scale = Math.max(1, scale - scaleFactor);
-    updateScale(mapContainer.clientWidth / 2, mapContainer.clientHeight / 2);
+    updateScale();
+    canZoom = false; // Блокируем дальнейшее уменьшение на полсекунды
+    setTimeout(() => {
+      canZoom = true;
+    }, 500);
   });
 
   mapContainer.addEventListener("wheel", (e) => {
     e.preventDefault();
+    if (!canZoom) return;
     const zoomFactor = e.deltaY < 0 ? scaleFactor : -scaleFactor;
     scale = Math.min(maxScale, Math.max(1, scale + zoomFactor));
-    const rect = videoWrapper.getBoundingClientRect();
-    const offsetX = (e.clientX - rect.left) / rect.width;
-    const offsetY = (e.clientY - rect.top) / rect.height;
-    updateScale(e.clientX, e.clientY, offsetX, offsetY);
+    updateScale();
+    canZoom = false; // Блокируем дальнейшее увеличение/уменьшение на полсекунды
+    setTimeout(() => {
+      canZoom = true;
+    }, 500);
   });
 
-  mapContainer.addEventListener("touchstart", handleTouchStart, false);
-  mapContainer.addEventListener("touchmove", handleTouchMove, false);
-  mapContainer.addEventListener("touchend", handleTouchEnd, false);
-
-  let x1 = null;
-  let y1 = null;
-  let x2 = null;
-  let y2 = null;
-  let initialDistance = null;
-
-  function handleTouchStart(e) {
-    const touches = e.touches;
-    if (touches.length === 2) {
-      x1 = touches[0].clientX;
-      y1 = touches[0].clientY;
-      x2 = touches[1].clientX;
-      y2 = touches[1].clientY;
-      initialDistance = getDistance(x1, y1, x2, y2);
-    } else if (touches.length === 1) {
-      isDragging = true;
-      startX = touches[0].clientX;
-      startY = touches[0].clientY;
-      initialX = videoWrapper.offsetLeft;
-      initialY = videoWrapper.offsetTop;
-      mapContainer.style.cursor = "grabbing";
-    }
-  }
-
-  function handleTouchMove(e) {
-    const touches = e.touches;
-    if (touches.length === 2) {
-      const newX1 = touches[0].clientX;
-      const newY1 = touches[0].clientY;
-      const newX2 = touches[1].clientX;
-      const newY2 = touches[1].clientY;
-      const newDistance = getDistance(newX1, newY1, newX2, newY2);
-
-      if (initialDistance) {
-        const zoomFactor =
-          ((newDistance - initialDistance) / initialDistance) * scaleFactor;
-        scale = Math.min(maxScale, Math.max(1, scale + zoomFactor));
-        const midX = (newX1 + newX2) / 2;
-        const midY = (newY1 + newY2) / 2;
-        const rect = videoWrapper.getBoundingClientRect();
-        const offsetX = (midX - rect.left) / rect.width;
-        const offsetY = (midY - rect.top) / rect.height;
-        updateScale(midX, midY, offsetX, offsetY);
-        initialDistance = newDistance;
-      }
-    } else if (touches.length === 1 && isDragging) {
-      const x = touches[0].clientX - startX + initialX;
-      const y = touches[0].clientY - startY + initialY;
-      videoWrapper.style.left = `${x}px`;
-      videoWrapper.style.top = `${y}px`;
-      constrainMap();
-    }
-  }
-
-  function handleTouchEnd(e) {
-    if (e.timeStamp - lastTouchEnd <= 300) {
-      e.preventDefault();
-      return;
-    }
-    lastTouchEnd = e.timeStamp;
-    isDragging = false;
-    mapContainer.style.cursor = "grab";
-  }
-
-  function getDistance(x1, y1, x2, y2) {
-    return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-  }
-
-  function updateScale(centerX, centerY, offsetX = 0.5, offsetY = 0.5) {
+  function updateScale() {
     const rect = videoWrapper.getBoundingClientRect();
     const newWidth = rect.width * scale;
     const newHeight = rect.height * scale;
-    const deltaX = (centerX - rect.left) * (scale - 1);
-    const deltaY = (centerY - rect.top) * (scale - 1);
     videoWrapper.style.transform = `scale(${scale})`;
-    videoWrapper.style.left = `${initialX - deltaX}px`;
-    videoWrapper.style.top = `${initialY - deltaY}px`;
+
+    // Центрирование по середине контейнера
+    const offsetX = (mapContainer.clientWidth - newWidth) / 2;
+    const offsetY = (mapContainer.clientHeight - newHeight) / 2;
+    videoWrapper.style.left = `${offsetX}px`;
+    videoWrapper.style.top = `${offsetY}px`;
+
     points.forEach((point) => {
       const x = parseFloat(point.getAttribute("data-x"));
       const y = parseFloat(point.getAttribute("data-y"));
@@ -224,4 +169,71 @@ document.addEventListener("DOMContentLoaded", () => {
     isDragging = false;
     mapContainer.style.cursor = "grab";
   });
+
+  mapContainer.addEventListener("touchstart", handleTouchStart, false);
+  mapContainer.addEventListener("touchmove", handleTouchMove, false);
+  mapContainer.addEventListener("touchend", handleTouchEnd, false);
+
+  let x1 = null;
+  let y1 = null;
+  let x2 = null;
+  let y2 = null;
+  let initialDistance = null;
+
+  function handleTouchStart(e) {
+    const touches = e.touches;
+    if (touches.length === 2) {
+      x1 = touches[0].clientX;
+      y1 = touches[0].clientY;
+      x2 = touches[1].clientX;
+      y2 = touches[1].clientY;
+      initialDistance = getDistance(x1, y1, x2, y2);
+    } else if (touches.length === 1) {
+      isDragging = true;
+      startX = touches[0].clientX;
+      startY = touches[0].clientY;
+      initialX = videoWrapper.offsetLeft;
+      initialY = videoWrapper.offsetTop;
+      mapContainer.style.cursor = "grabbing";
+    }
+  }
+
+  function handleTouchMove(e) {
+    const touches = e.touches;
+    if (touches.length === 2) {
+      const newX1 = touches[0].clientX;
+      const newY1 = touches[0].clientY;
+      const newX2 = touches[1].clientX;
+      const newY2 = touches[1].clientY;
+      const newDistance = getDistance(newX1, newY1, newX2, newY2);
+
+      if (initialDistance) {
+        const zoomFactor =
+          ((newDistance - initialDistance) / initialDistance) * scaleFactor;
+        scale = Math.min(maxScale, Math.max(1, scale + zoomFactor));
+        updateScale();
+        initialDistance = newDistance;
+      }
+    } else if (touches.length === 1 && isDragging) {
+      const x = touches[0].clientX - startX + initialX;
+      const y = touches[0].clientY - startY + initialY;
+      videoWrapper.style.left = `${x}px`;
+      videoWrapper.style.top = `${y}px`;
+      constrainMap();
+    }
+  }
+
+  function handleTouchEnd(e) {
+    if (e.timeStamp - lastTouchEnd <= 300) {
+      e.preventDefault();
+      return;
+    }
+    lastTouchEnd = e.timeStamp;
+    isDragging = false;
+    mapContainer.style.cursor = "grab";
+  }
+
+  function getDistance(x1, y1, x2, y2) {
+    return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+  }
 });
